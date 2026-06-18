1d3f99fac616256686df3bdae52bd2995c19295a
jordy25519 <beauchjord@gmail.com>
2026-06-18 15:09:35 +0800

    fix(vaults): restore production admin id to canonical 4wbNjWb (was a local keypair)

--- programs/vaults/src/constants.rs
+++ programs/vaults/src/constants.rs
@@ -8,7 +8,7 @@ pub const ONE_WEEK: i64 = ONE_DAY * 7;
 pub mod admin {
     use anchor_lang::prelude::{pubkey, Pubkey};
     #[cfg(not(feature = "anchor-test"))]
-    pub const ID: Pubkey = pubkey!("GiMXQkJXLVjScmQDkoLJShBJpTh9SDPvT2AZQq8NyEBf");
+    pub const ID: Pubkey = pubkey!("4wbNjWbj3kPDbyKnSq8SXVEtAJw4uzE8mJ2QwuK1BCYZ");
 
     #[cfg(feature = "anchor-test")]
     pub const ID: Pubkey = pubkey!("45HdJoU4aHmRzYBpd2zSvjvyfMUdzbrgBDkqLLcW45yA");

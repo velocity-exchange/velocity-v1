05b68a485fb211f55b78b88a956be7a20332389f
Chester Sim <chestersimdy@gmail.com>
2026-06-18 11:03:28 +0800

    revert 1

--- sdk/src/math/exchangeStatus.ts
+++ sdk/src/math/exchangeStatus.ts
@@ -127,3 +127,10 @@ export function isMarketConfigFlagSet(
 ): boolean {
 	return (marketConfig & flag) > 0;
 }
+
+export function isFormulaicKUpdateDisabled(market: PerpMarketAccount): boolean {
+	return isMarketConfigFlagSet(
+		market.marketConfig,
+		MarketConfigFlag.DISABLE_FORMULAIC_K_UPDATE
+	);
+}

375dd48827e3a86e8b477e331868c2f88a722508
Chester Sim <chestersimdy@gmail.com>
2026-08-28 10:13:05 +0800

    fix: correct default value assignment for maxUsersToConsider in UserPnlSettlerBot

--- apps/keeper-bots-v2/src/bots/userPnlSettler.ts
+++ apps/keeper-bots-v2/src/bots/userPnlSettler.ts
@@ -153,7 +153,7 @@ export class UserPnlSettlerBot implements Bot {
 		this.minPnlToSettle = new BN(
 			Math.abs(Number(config.settlePnlThresholdUsdc) ?? 10) * -1
 		).mul(QUOTE_PRECISION);
-		this.maxUsersToConsider = Number(config.maxUsersToConsider) ?? 50;
+		this.maxUsersToConsider = Number(config.maxUsersToConsider ?? 50);
 		this.globalConfig = globalConfig;
 
 		this.velocityClient = velocityClient;
